4f0016c01e0e2afa19290f9c4f2772fa8d33f347
Hyeon Kim <simnalamburt@gmail.com>
2014-09-26 00:09:46 +0900

    Initial commit
    Imported skeletal files

---
+++ hanoi.c
@@ -0,0 +1,8 @@
+#include "hanoi.h"
+
+/* You can add any functions */
+
+void hanoi(char *input)
+{
+	/* Implement here */
+}
---
+++ hanoi.h
@@ -0,0 +1,4 @@
+#include<stdio.h>
+#include<sys/time.h>
+
+void hanoi(char *);
