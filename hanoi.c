a71c3c939b2795e48c4589f9c56d5deae8faf5e4
Hyeon Kim <simnalamburt@gmail.com>
2014-09-26 15:25:44 +0900

    Misc: 코드 정리

--- hanoi.c
+++ hanoi.c
@@ -13,25 +13,29 @@ static double time_in_float() {
 
 
 // 재귀버전: solve1(n)
-static int i;
-static void solve_rec(int count, int src, int dst) {
-  int tmp;
+static int _i;
 
+//  count : 옮길 판들의 갯수                   (자연수)
+//    src : 옮길 판들의 출발지                 (0, 1, 2)
+//    dst : 옮길 판들의 목적지                 (0, 1, 2)
+// number : 옮길 판들 가운데 제일 큰 판의 번호 (0, 1, ... count)
+static void solve_rec(int count, int src, int dst, int number) {
   if (count == 1) {
-    printf("%d: Disk %d from %c to %c\n", i++, count, src + 'a', dst + 'a');
+    printf("%d: Disk %d from %c to %c\n", _i++, number, src + 'a', dst + 'a');
   } else {
-    tmp = 3 - src - dst;
-    solve_rec(count - 1, src, tmp);
-    printf("%d: Disk %d from %c to %c\n", i++, count, src + 'a', dst + 'a');
-    solve_rec(count - 1, tmp, dst);
+    int tmp = 3 - src - dst;
+
+    solve_rec(count - 1, src, tmp, number - 1);
+    solve_rec(1,         src, dst, number);
+    solve_rec(count - 1, tmp, dst, number - 1);
   }
 }
 
 static void solve1(int count) {
   printf("N=%d, Time stamp: %lf\n", count, time_in_float());
 
-  i = 1;
-  solve_rec(count, 0, 1);
+  _i = 1;
+  solve_rec(count, 0, 1, count);
 
   putchar('\n');
 }
@@ -39,9 +43,9 @@ static void solve1(int count) {
 
 // 반복문버전: solve2(n)
 typedef struct job {
-  int count;  // 옮길 판들의 갯수 (자연수)
-  int src;    // 옮길 판들의 출발지 (0, 1, 2)
-  int dst;    // 옮길 판들의 목적지 (0, 1, 2)
+  int count;  // 옮길 판들의 갯수                   (자연수)
+  int src;    // 옮길 판들의 출발지                 (0, 1, 2)
+  int dst;    // 옮길 판들의 목적지                 (0, 1, 2)
   int number; // 옮길 판들 가운데 제일 큰 판의 번호 (0, 1, ... count)
 } job;
 
@@ -65,11 +69,10 @@ static job pop() {
 }
 
 static void solve2(int count) {
-  int i = 1, third;
-
   printf("N=%d, Time stamp: %lf\n", count, time_in_float());
   push(count, 0, 1, count);
 
+  int i = 1;
   do {
     job job = pop();
 
@@ -77,11 +80,11 @@ static void solve2(int count) {
       printf("%d: Disk %d from %c to %c\n",
         i++, job.number, job.src + 'a', job.dst + 'a');
     } else {
-      third = 3 - job.src - job.dst;
+      int tmp = 3 - job.src - job.dst;
 
-      push(job.count-1, third, job.dst, job.count-1);   // Step 3
-      push(1, job.src, job.dst, job.count);         // Step 2
-      push(job.count-1, job.src, third, job.count-1);   // Step 1
+      push(job.count - 1, tmp,     job.dst, job.number - 1);  // Step 3
+      push(1,             job.src, job.dst, job.number);      // Step 2
+      push(job.count - 1, job.src, tmp,     job.number - 1);  // Step 1
     }
 
   } while (!empty());
